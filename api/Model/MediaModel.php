daf951d4f2b933799f194f474d63dedd82a8614e
germondai <bruno.krocek@gmail.com>
2024-06-20 10:56:31 +0200

    media model and upload works

---
+++ api/Model/MediaModel.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Model;
+
+use Api\Controller\Api;
+use Api\Entity\Media;
+use Utils\Helper;
+
+class MediaModel extends Api
+{
+    public function actionUploadFile()
+    {
+        $res = [];
+        foreach ($_FILES as $file)
+            $res[] = Helper::uploadFile($file, Helper::getBasePath() . 'public/uploads/', 80);
+        return $res;
+    }
+}
