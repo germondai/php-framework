aa66f316d30a2e16d96e55464e78d2d45e5b3246
germondai <bruno.krocek@gmail.com>
2024-06-21 09:34:31 +0200

    transparentize only specific formats

--- src/Utils/Helper.php
+++ src/Utils/Helper.php
@@ -106,11 +106,12 @@ class Helper
                         if (!empty($img)) {
                             $dst = imagecreatetruecolor($newW, $newH);
 
-                            // transparentize
-                            imagealphablending($dst, false);
-                            imagesavealpha($dst, true);
-                            $transparent = imagecolorallocatealpha($dst, 0, 0, 0, 127);
-                            imagefilledrectangle($dst, 0, 0, $newW, $newH, $transparent);
+                            if (in_array($ext, ['png', 'gif', 'bmp', 'tiff', 'jp2'])) {
+                                imagealphablending($dst, false);
+                                imagesavealpha($dst, true);
+                                $transparent = imagecolorallocatealpha($dst, 0, 0, 0, 127);
+                                imagefilledrectangle($dst, 0, 0, $newW, $newH, $transparent);
+                            }
 
                             imagecopyresampled($dst, $img, 0, 0, 0, 0, $newW, $newH, $imgW, $imgH);
 
