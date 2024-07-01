482b8c226457f1f9319901fde887028bb19b8db6
germondai <bruno.krocek@gmail.com>
2024-07-01 11:43:10 +0200

    added blade components

---
+++ app/View/Components/example.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center gap-2 p-4 bg-slate-950 rounded-xl shadow shadow-white">
+    <b>Blade Component with slot</b>
+    <p>{!! $slot !!}</p>
+</div>
--- app/View/index.blade.php
+++ app/View/index.blade.php
@@ -1,7 +1,9 @@
 <main class="w-full h-screen grid place-items-center">
-    <h1><?= $greeting ?></h1>
-
-    @if (!empty($greeting))
-        {{$greeting}}
-    @endif
+    <div class="flex flex-col items-center gap-8">
+        <h1>{{ $greeting }}</h1>
+    
+        @component('components.example')
+            Component slot passed from <b>index</b>!
+        @endcomponent
+    </div>    
 </main>
